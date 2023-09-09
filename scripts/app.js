dc3fcf47d9aa7923d4da29307756416c220d572e
ajay <ap221882@gmail.com>
2023-09-09 17:07:12 +0530

    request persistence, check quota

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,32 @@
-import Menu from './Menu.js';
-import Order from './Order.js';
-import Router from './Router.js';
+import Menu from "./Menu.js";
+import Order from "./Order.js";
+import Router from "./Router.js";
+
+//> Request persistence storage
+
+(async function () {
+  if (navigator.storage && navigator.storage.persist) {
+    if (!(await navigator.storage.persist())) {
+      const result = await navigator.storage.persist();
+
+      console.log(`Persistent request: returned ${result}`);
+    }
+  }
+})();
+
+//> check for estimated quota
+
+(async function () {
+  if (navigator.storage && navigator.storage.estimate) {
+    const quota = await navigator.storage.estimate();
+
+    console.log(`Estimated quota: ${quota.quota}`);
+    console.log(`Quota used: ${quota.usage}`);
+  }
+})();
 
 window.addEventListener("DOMContentLoaded", () => {
-    Router.init();
-    Menu.load();
-    Order.render();
- } );
+  Router.init();
+  Menu.load();
+  Order.render();
+});
